4f76a925399624620795ed821870438731be75e6
marvin-j97 <marvin.janke.97@gmail.com>
2022-12-13 16:25:21 +0100

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ export default defineConfig({
     coverage: {
       provider: "istanbul",
       reporter: ["json", "html", "text"],
+      // TODO: "100" option is not supported (yet) for istanbul
+      lines: 100,
+      functions: 100,
+      branches: 100,
+      statements: 100,
     },
   },
 });
